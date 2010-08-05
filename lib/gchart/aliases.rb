aab1dd42c922a1ccdc90b29761780eb63d7b3aea
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2010-08-06 04:37:02 +0800

    Added alias "slice_colors"

--- lib/gchart/aliases.rb
+++ lib/gchart/aliases.rb
@@ -7,6 +7,7 @@ class Gchart
   alias_method :bar_color=, :bar_colors=
   alias_method :line_colors=, :bar_colors=
   alias_method :line_color=, :bar_colors=
+  alias_method :slice_colors=, :bar_colors=
   alias_method :labels=, :legend=
   alias_method :horizontal?, :horizontal
   alias_method :grouped?, :grouped
