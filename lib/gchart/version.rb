fdbbff60843e06b81860ee2e0cbc37367ed34124
Matt Aimonetti <mattaimonetti@gmail.com>
2011-04-13 23:29:48 -0700

    bumped the version

--- lib/gchart/version.rb
+++ lib/gchart/version.rb
@@ -1,8 +1,8 @@
 module GchartInfo #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 5
-    TINY  = 4
+    MINOR = 6
+    TINY  = 2
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
